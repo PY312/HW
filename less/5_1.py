569adbb9754125dfb680361da16f74b11e7fbae9
PY312 <83508520+PY312@users.noreply.github.com>
2021-05-19 10:34:05 +0600

    словарь

--- less/5_1.py
+++ less/5_1.py
@@ -17,12 +17,12 @@ en_kg = {'money': 'акча',
          'red': 'кызыл',
          'girl':'кыз'}
 
-dictionary = ['English', en_kg, 'Kyrgyz'], \
-             ['English', en_ru, 'Russian'], \
-             ['Кыргызский',kg_ru, 'Russian'] ,\
-             ['Кыргызский',kg_en, 'English'], \
-             ['Русский', ru_en, 'English'],\
-             ['Русский', ru_kg, 'Kyrgyz']
+dictionary = ['English', en_kg, 'на кыргызском языке'], \
+             ['English', en_ru, 'на русском языке'], \
+             ['на кыргызском языке',kg_ru, 'на русском языке'] ,\
+             ['на кыргызском языке',kg_en, 'English'], \
+             ['на русском языке', ru_en, 'English'],\
+             ['на русском языке', ru_kg, 'на кыргызском языке']
 
 while True:
     word = input('Введите слово для перевода :')
@@ -32,3 +32,17 @@ while True:
                 print("Язык оригинал: " + i[0])
                 print("Язык перевода: " + i[2])
                 print(i[1][key])
+            else:
+                print('Такого слова нет в словаре')
+                action = input('Хотите ввести новое слово словарь? Y/N ')
+                if action.lower() == 'y':
+                    lang = input('Выберите язык оригинала en, ru, kg    ')
+                    if lang == 'en':
+                        en_ru[word] = input(f'Введите перевод к слову {word} на русском языке ')
+                        en_kg[word] = input(f'Введите перевод к слову {word} на кыргызском языке ')
+                    elif lang == 'ru':
+                        ru_en[word] = input(f'Введите перевод к слову {word} на английском языке ')
+                        ru_kg[word] = input(f'Введите перевод к слову {word} на кыргызском языке ')
+                    elif lang == 'kg':
+                        kg_ru[word] = input(f'Введите перевод к слову {word} на русском языке ')
+                        kg_en[word] = input(f'Введите перевод к слову {word} на английском языке ')
